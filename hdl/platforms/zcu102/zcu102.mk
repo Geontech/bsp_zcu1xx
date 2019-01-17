c24077949dde49c618244088fa8b2b935c2d32f3
dbanks <dbanks@geontech.com>
2019-01-17 15:15:28 -0500

    Added the HdlRccPlatform binding to xilinx18_2 for both zcu102 and zcu111 to allow building with the --hdl-rcc-platform option

--- hdl/platforms/zcu102/zcu102.mk
+++ hdl/platforms/zcu102/zcu102.mk
@@ -1,5 +1,6 @@
 # SET THIS VARIABLE to the part (die-speed-package, e.g. xc7z020-1-clg484) for the platform!
 HdlPart_zcu102=xczu9eg-2-ffvb1156-e
+HdlRccPlatform_zcu102=xilinx18_2
 # Set this variable to the names of any other component libraries with devices defined in this
 # platform. Do not use slashes.  If there is an hdl/devices library in this project, it will be
 # searched automatically, as will "devices" in any projects this project depends on.
--- hdl/platforms/zcu111/zcu111.mk
+++ hdl/platforms/zcu111/zcu111.mk
@@ -1,5 +1,6 @@
 # SET THIS VARIABLE to the part (die-speed-package, e.g. xc7z020-1-clg484) for the platform!
 HdlPart_zcu111=xczu28dr-2-ffvg1517-e
+HdlRccPlatform_zcu111=xilinx18_2
 # Set this variable to the names of any other component libraries with devices defined in this
 # platform. Do not use slashes.  If there is an hdl/devices library in this project, it will be
 # searched automatically, as will "devices" in any projects this project depends on.
